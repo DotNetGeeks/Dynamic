706685d05cbb3f8c7393ab53e00479347c68a706
dotnetgeeks <dotnetgeeks1718@gmail.com>
2023-06-03 18:49:39 -0600

    Dynamic ejemplo

---
+++ Dynamic/Program.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace Dynamic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           dynamic myVariable = "";
+           
+            string jsonString = @"{""menu"": {
+                                      ""id"": ""file"",
+                                      ""value"": ""File"",
+                                      ""popup"": {
+                                        ""menuitem"": [
+                                          {""value"": ""New"", ""onclick"": ""CreateNewDoc()""},
+                                          {""value"": ""Open"", ""onclick"": ""OpenDoc()""},
+                                          {""value"": ""Close"", ""onclick"": ""CloseDoc()""}
+                                        ]
+                                      }
+                                    }}";
+            
+            myVariable = JObject.Parse(jsonString);
+            Console.WriteLine(myVariable.menu.id);
+            Console.WriteLine(" ");
+            Console.WriteLine(myVariable.menu.popup);
+        }
+    }
+}
